7bb7bfef79cff0a8e7207b2fd10162e11a6a062e
Poommin.tk <poommin.tk@gmail.com>
2026-04-23 13:51:53 +0700

    Update ex3_1.py

--- Email-Lab02/ex3_1.py
+++ Email-Lab02/ex3_1.py
@@ -1,18 +1,17 @@
 import paramiko
 
-# ข้อมูลการเชื่อมต่อ [cite: 479, 498-502]
+# ข้อมูลการเชื่อมต่อ 
 SSH_HOST = 'digilab.sut.ac.th'
 SSH_PORT = 2222
-SSH_USER = 'student51' # เปลี่ยนเป็น username ของคุณ (เช่น student01-75) [cite: 501]
-SSH_PASS = 'yoi7UGygDAqO3z'
+SSH_USER = 'student51' # เปลี่ยนเป็น username ของคุณ (เช่น student01-75) 
+SSH_PASS = 'i7UGygDAqO3z' # เปลี่ยนเป็น password ของคุณ
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(SSH_HOST, port=SSH_PORT, username=SSH_USER, password=SSH_PASS, timeout=10)
-commands = ['hostname', 'whoami', 'df -h']
 
-for cmd in commands:
-    stdin, out, stderr = client.exec_command(cmd)
+for cmd in ['hostname', 'whoami', 'df -h']:
+    _, out, stderr = client.exec_command(cmd)
     print(f'=== {cmd} ===')
     print(out.read().decode().strip())
     
